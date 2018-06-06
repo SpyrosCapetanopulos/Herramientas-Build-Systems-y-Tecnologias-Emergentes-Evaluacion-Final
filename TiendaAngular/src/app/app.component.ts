98d1f24f2ddf8ab3038e9dff21e5de0c85ac372f
SpyrosCapetanopulos <thesone@icloud.com>
2018-06-06 17:33:11 -0400

    Add Bootstrap
    Se agrego bootstrap al desarrollo

--- TiendaAngular/src/app/app.component.ts
+++ TiendaAngular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PageLoginComponent } from './components/page-login/page-login.component';
+import { PageBarraComponent } from './components/page-barra/page-barra.component';
 
 @Component({
   selector: 'app-root',
